643ba8d093a747ff9749bc222eb5e8941a601471
danamnasr <101146257+danamnasr@users.noreply.github.com>
2022-08-11 22:06:48 -0400

    most updated

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    console.log("javascriptloaded");
+    //SHOW MODAL WITH FADEIN METHOD
+    setTimeout(() => {
+        $("#overlay").fadeIn('slow');
+        $("#modal").fadeIn('slow');
+    }, 2000);
+
+    //CLOSE MODAL WITH FADEOUT METHOD
+    $(document).click(function () {
+        var container = $("#container");
+        if (!container.is(event.target) && !container.has(event.target).length) {
+            $("#modal").fadeOut(200);
+            $("#overlay").fadeOut(200);
+        }
+    });
+});
